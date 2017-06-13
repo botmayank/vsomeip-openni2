4ababa57d550d43ddf5c5466879b132a224a279c
hyp <botmayank@gmail.com>
2017-06-13 18:02:26 +0200

    added boost udp application and modified service/client for image transfer

--- src/client-example.cpp
+++ src/client-example.cpp
@@ -15,7 +15,7 @@
 #define SAMPLE_EVENTGROUP_ID 0x0666
 #define SAMPLE_EVENT_ID 0x0667
 
-// #define PUB_SUB
+#define PUB_SUB
 
 std::shared_ptr <vsomeip::application> app;
 std::mutex mutex;
@@ -55,7 +55,9 @@ void run() {
 
   std::set<vsomeip::eventgroup_t> its_groups;
   its_groups.insert(SAMPLE_EVENTGROUP_ID);
+  std::cout<< "Requesting event" << std::endl;
   app->request_event(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID, its_groups, true);
+  std::cout<< "Subscribing to event" << std::endl;
   app->subscribe(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENTGROUP_ID);
 
 }
@@ -119,15 +121,16 @@ void on_availability(vsomeip::service_t _service, vsomeip::instance_t _instance,
 }
 
 int main() {
-
     app = vsomeip::runtime::get()->create_application("Hello");
     app->init();
     app->register_availability_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, on_availability);
     app->request_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
-    
+#ifndef PUB_SUB
     app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_message);
-    // app->register_message_handler(vsomeip::ANY_SERVICE, vsomeip::ANY_INSTANCE, vsomeip::ANY_METHOD, on_message);
-
+#else
+     std::cout << "Registering message handler" << std::endl;
+     app->register_message_handler(vsomeip::ANY_SERVICE, vsomeip::ANY_INSTANCE, vsomeip::ANY_METHOD, on_message);
+#endif
     std::thread sender(run);
     app->start();
 }
---
+++ src/udp_client.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+class UDPClient
+{
+public:
+	UDPClient(
+		boost::asio::io_service& io_service, 
+		const std::string& host, 
+		const std::string& port
+	) : io_service_(io_service), socket_(io_service, udp::endpoint(udp::v4(), 0)) {
+		udp::resolver resolver(io_service_);
+		udp::resolver::query query(udp::v4(), host, port, boost::asio::ip::resolver_query_base::flags());
+		udp::resolver::iterator iter = resolver.resolve(query);
+		endpoint_ = *iter;
+	}
+
+	~UDPClient()
+	{
+		socket_.close();
+	}
+
+	void send(const std::string& msg) {
+		socket_.send_to(boost::asio::buffer(msg, msg.size()), endpoint_);
+	}
+
+private:
+	boost::asio::io_service& io_service_;
+	udp::socket socket_;
+	udp::endpoint endpoint_;
+};
+
+int main()
+{
+	boost::asio::io_service io_service;
+	UDPClient client(io_service, "192.168.0.100", "1337");
+	std::cout <<"Sending Hello World! to pi:1337" << std::endl;
+	client.send("Hello, World!");
+}
